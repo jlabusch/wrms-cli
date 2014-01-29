a070f2425df7f9fac35c014ef27c21e945b48954
Jacques Labuschagne <jlabusch@acm.org>
2014-01-29 14:52:09 +1300

    Added example test hook usage

--- test/basic.js
+++ test/basic.js
@@ -11,6 +11,12 @@ var test_f = default_test_f;
 function test_completion(data){
     test_f(data);
 }
+
+var child_counters = {
+    loaded: 0,
+    cached: 0
+};
+
 var env = {
     config: {
         on_completion: 'test_completion',
@@ -21,6 +27,12 @@ var env = {
         debug: false
     },
     db: db,
+    __load_child: {
+        entry: null,
+        after_fetch: null,
+        pre_exit_new: function(){ child_counters.loaded++; },
+        pre_exit_cached: function(w){ child_counters.cached++; }
+    },
     test_completion: test_completion
 };
 
@@ -30,20 +42,28 @@ describe('hello world', function(){
     describe('basic execution', function(){
         it('should complete', function(done){
             test_f = function(wrs){
+                child_counters.loaded.should.equal(17);
+                child_counters.cached.should.equal(1);
                 test.mark_end(1);
                 done();
             };
             test.mark_start(1);
+            child_counters.loaded = 0;
+            child_counters.cached = 0;
             db.init().then(bind(wrms_cli.run, [94701])).fail(die);
         });
         it('should find WR 94701', function(done){
             test_f = function(wrs){
                 wrs[94701].brief.should.equal('Iteration Zoolander');
                 wrs[94701].status.should.equal('Finished');
+                child_counters.loaded.should.equal(17);
+                child_counters.cached.should.equal(1);
                 test.mark_end(2);
                 done();
             };
             test.mark_start(2);
+            child_counters.loaded = 0;
+            child_counters.cached = 0;
             test.start_after([1], function(){
                 db.init().then(bind(wrms_cli.run, [94701])).fail(die);
             });
